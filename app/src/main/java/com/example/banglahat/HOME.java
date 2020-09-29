15c9f24b6e3a3b642b18d078bcca187a64ee1d1e
sohamjuhin <juhinsohamdas@gmail.com>
2020-09-29 16:46:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/banglahat/HOME.java
@@ -0,0 +1,14 @@
+package com.example.banglahat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HOME extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_h_o_m_e);
+
+    }
+}
